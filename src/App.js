3aa00dd7291f431164ed6a5e9e26e0d7777cd1a3
SergioRt1 <sergio200035@gmail.com>
2019-02-06 00:22:23 -0500

    Part 2: Enable App Navigation

--- src/App.js
+++ src/App.js
@@ -3,21 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import {Login} from "./component/Login";
 import {TodoApp} from "./TodoApp";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+const LoginView = () => (
+    <Login/>
+);
+
+const TodoView = () => (
+    <TodoApp/>
+);
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state={isLoggedIn: false}
+    }
+
     render() {
+
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">TODO React App</h1>
-                </header>
-                <br/>
-                <br/>
-                <TodoApp/>
-                <Login/>
-            </div>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
+
+                    <br/>
+                    <br/>
+
+                    <ul>
+                        {this.state.isLoggedIn?
+                            <li><Link to="/todo">Todo</Link></li> : <li><Link to="/">Login</Link></li>}
+                    </ul>
+
+                    <div>
+                        {this.state.isLoggedIn?
+                            < Route path = "/todo" component={TodoView}/> : <Route exact path="/" component={LoginView}/>
+                        }
+                    </div>
+                </div>
+            </Router>
         );
     }
 
